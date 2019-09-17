813c98931a0f05396a970c22f068a96a521c85d3
ubermoc <ubermoc@gmail.com>
2019-09-16 19:00:41 -0700

    Finished

---
+++ client/src/components/useDarkMode.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
+
+export const useDarkMode = () => {
+  const [storedValue, setstoredValue] = useLocalStorage('darkMode');
+
+  useEffect(() => {
+    if (storedValue) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [storedValue]);
+  return [storedValue, setstoredValue];
+};
